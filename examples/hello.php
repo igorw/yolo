4b1d61afbc1db1a688920aa019c2ae15d6b60610
Igor Wiedler <igor@wiedler.ch>
2013-02-07 06:05:22 +0100

    Introduce convenience Application object

--- examples/hello.php
+++ examples/hello.php
@@ -5,16 +5,14 @@ require __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-$container = Yolo\Factory::createContainer();
-$builder = $container->get('route_builder');
+$app = new Yolo\Application();
 
-$builder->get('hello', '/', function (Request $request) {
+$app->get('hello', '/', function (Request $request) {
     return new Response("Hallo welt, got swag yo!\n");
 });
 
-$builder->get('error', '/error', function (Request $request) {
+$app->get('error', '/error', function (Request $request) {
     throw new \Exception('Holy crap, explosion!');
 });
 
-$front = $container->get('front_controller');
-$front->run();
+$app->run();
