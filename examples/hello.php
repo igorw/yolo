736d4e4940fa964b3daab2e2cbb00af3df5ef595
Igor Wiedler <igor@wiedler.ch>
2013-02-07 07:15:58 +0100

    Move error handling examples to separate file

--- examples/hello.php
+++ examples/hello.php
@@ -4,10 +4,6 @@ require __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-
-class GracefulException extends \Exception {}
 
 $container = Yolo\createContainer(
     [
@@ -22,27 +18,5 @@ $app = new Yolo\Application($container);
 $app->get('hello', '/', function (Request $request) {
     return new Response("Hallo welt, got swag yo!\n");
 });
-
-$app->get('explosion', '/500', function (Request $request) {
-    throw new \Exception('Holy crap, explosion!');
-});
-
-$app->get('bad_request', '/400', function (Request $request) {
-    throw new BadRequestHttpException('Janz schlimm.');
-});
-
-$app->get('graceful', '/graceful', function (Request $request) {
-    throw new GracefulException('Fuuuuuuu!');
-});
-
-$container
-    ->get('dispatcher')
-    ->addListener(KernelEvents::EXCEPTION, function ($event) {
-        $e = $event->getException();
-        if ($e instanceof GracefulException) {
-            $message = sprintf("Exception '%s' handled exception gracefully.\n", $e->getMessage());
-            $event->setResponse(new Response($message));
-        }
-    });
 
 $app->run();
