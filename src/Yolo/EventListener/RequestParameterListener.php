fd87a07bfa6e1d44531ea2dc2b9f389e680d59e2
Igor Wiedler <igor@wiedler.ch>
2013-02-13 04:52:56 +0100

    Split implicit controller into request param and string response listeners

--- src/Yolo/EventListener/RequestParameterListener.php
+++ src/Yolo/EventListener/RequestParameterListener.php
@@ -4,11 +4,10 @@ namespace Yolo\EventListener;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class ImplicitControllerListener implements EventSubscriberInterface
+class RequestParameterListener implements EventSubscriberInterface
 {
     public function onKernelRequest(GetResponseEvent $event)
     {
@@ -18,17 +17,10 @@ class ImplicitControllerListener implements EventSubscriberInterface
         }
     }
 
-    public function onKernelView(GetResponseForControllerResultEvent $event)
-    {
-        $result = $event->getControllerResult();
-        $event->setResponse(new Response((string) $result));
-    }
-
     public static function getSubscribedEvents()
     {
         return [
             KernelEvents::REQUEST   => ['onKernelRequest'],
-            KernelEvents::VIEW      => [['onKernelView', -512]],
         ];
     }
 }
