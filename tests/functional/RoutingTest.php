c2edbfa5632ceb3931a122aaad27e690f9a9d72c
Igor Wiedler <igor@wiedler.ch>
2013-02-13 05:04:42 +0100

    Add functional tests covering request parameter and string response listeners

--- tests/functional/RoutingTest.php
+++ tests/functional/RoutingTest.php
@@ -26,7 +26,7 @@ class RoutingTest extends \PHPUnit_Framework_TestCase
         $this->assertSame("Hallo welt, got swag yo!\n", $response->getContent());
     }
 
-    public function testSetRequirement()
+    public function testSetRouteRequirement()
     {
         $app = new Application();
 
@@ -35,11 +35,42 @@ class RoutingTest extends \PHPUnit_Framework_TestCase
         })
         ->setRequirement('_method', 'POST');
 
-        $kernel = $app->getContainer()->get('http_kernel');
         $request = Request::create('/', 'POST');
+        $this->assertResponse($app, $request, 'ok', 201);
+    }
+
+    /** @test */
+    public function controllerShouldNotRequireRequestTypeHint()
+    {
+        $app = new Application();
+
+        $app->get('/', function ($request) {
+            return new Response('ok', 200);
+        });
+
+        $request = Request::create('/');
+        $this->assertResponse($app, $request, 'ok', 200);
+    }
+
+    /** @test */
+    public function controllerShouldConvertStringToResponse()
+    {
+        $app = new Application();
+
+        $app->get('/', function (Request $request) {
+            return 'ok';
+        });
+
+        $request = Request::create('/');
+        $this->assertResponse($app, $request, 'ok', 200);
+    }
+
+    private function assertResponse(Application $app, Request $request, $content, $status)
+    {
+        $kernel = $app->getContainer()->get('http_kernel');
         $response = $kernel->handle($request, HttpKernelInterface::MASTER_REQUEST, false);
 
-        $this->assertSame(201, $response->getStatusCode());
-        $this->assertSame('ok', $response->getContent());
+        $this->assertSame($content, $response->getContent());
+        $this->assertSame($status, $response->getStatusCode());
     }
 }
