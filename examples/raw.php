0b8d0a23010cafc715b3882120a3a8ce5034a71e
Igor Wiedler <igor@wiedler.ch>
2013-03-10 04:47:34 +0100

    Raw example not using Application

---
+++ examples/raw.php
@@ -0,0 +1,17 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$container = Yolo\createContainer();
+
+$builder = $container->get('route_builder');
+
+$builder->get('/', function ($request) {
+    return "Hallo welt, got swag yo!\n";
+});
+
+$front = $container->get('front_controller');
+$front->run();
