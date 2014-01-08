6647bd1017b7ba55ca1d2c581c0da7a7265840df
Igor Wiedler <igor@wiedler.ch>
2014-01-08 16:52:43 +0100

    move to PSR-4, woohoo

--- src/DependencyInjection/YoloExtension.php
+++ src/DependencyInjection/YoloExtension.php
@@ -11,7 +11,7 @@ class YoloExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../../config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.yml');
     }
 }
