987ba351a066136cab4280f0bb31329c420ab6ab
Radek Slupik <rightfold@gmail.com>
2014-11-13 12:34:23 +0100

    Make it actually live once, not never

--- src/yolo.php
+++ src/yolo.php
@@ -1,2 +1,11 @@
+(9 sloc)  0.317 kb RawBlameHistory  
 <?php #YOLO
-unlink(__FILE__);
+namespace yolo;
+class_alias('Symfony\Component\HttpFoundation\Request', Request::class);
+class_alias('Symfony\Component\HttpFoundation\Response', Response::class);
+function yolo($controller) {
+    $request = Request::createFromGlobals();
+    $response = $controller($request);
+    $response->send();
+    unlink(__FILE__);
+}
