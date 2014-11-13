f84c16c3cbe2028165b438bc0252a793443727ca
Radek Slupik <rightfold@gmail.com>
2014-11-13 12:30:33 +0100

    Make yolo live only once

--- src/yolo.php
+++ src/yolo.php
@@ -1,12 +1,2 @@
 <?php #YOLO
-
-namespace yolo;
-
-class_alias('Symfony\Component\HttpFoundation\Request', Request::class);
-class_alias('Symfony\Component\HttpFoundation\Response', Response::class);
-
-function yolo($controller) {
-    $request = Request::createFromGlobals();
-    $response = $controller($request);
-    $response->send();
-}
+unlink(__FILE__);
