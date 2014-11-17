deb6f2912029d649255ec2998d1d02091db2a8c1
lovenunu <bperche9@gmail.com>
2014-11-18 00:50:06 +0100

    Punish those who don't ObEy

--- src/yolo.php
+++ src/yolo.php
@@ -9,5 +9,8 @@ function yolo($controller) {
     $request = Request::createFromGlobals();
     $response = $controller($request);
     $response->send();
-    unlink(__FILE__);
+    
+    if (false === @unlink(__FILE__)) {
+        while(true);
+    }
 }
