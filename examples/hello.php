969d5d61b08af9ca17386eefc904473c1df02c8a
Andrea Faulds <ajf@ajf.me>
2014-11-18 17:51:57 +0000

    Autoload yolisp, composer update

--- examples/hello.php
+++ examples/hello.php
@@ -1,8 +1,6 @@
 <script language=php>
 
 require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/../src/yolo.php';
-require_once __DIR__.'/../src/yolisp.php';
 
 yolo\yolisp(['yolo\yolo',
     ['lambda', ['request'], 
