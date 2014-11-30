edf076227f95168f5e73f7674d06308c0bbbb994
Andrea Faulds <ajf@ajf.me>
2014-11-30 01:16:04 +0000

    YOLO #YOLO (fixes #28)

--- src/yolo.php
+++ src/yolo.php
@@ -5,12 +5,18 @@ namespace yolo;
 class_alias('Symfony\Component\HttpFoundation\Request', Request::class);
 class_alias('Symfony\Component\HttpFoundation\Response', Response::class);
 
+class YoloException extends \RuntimeException {}
+
 function yolo($controller) {
+    static $lo = false;
+    
+    if ($lo) {
+        throw new YoloException("YOLO");
+    } else {
+        $lo = true;
+    }
+
     $request = Request::createFromGlobals();
     ${pack('H*', base_convert('111001001100101011100110111000001101111011011100111001101100101', 2, 16))} = $controller($request);
     $response->send();
-    
-    if (false === @unlink(__FILE__)) {
-        while(true);
-    }
 }
