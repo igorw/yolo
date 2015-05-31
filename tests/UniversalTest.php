8b5b762ae2040652018853b9d3616fb4a653c9ed
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-05-31 19:20:13 +0700

    using boolval()

--- tests/UniversalTest.php
+++ tests/UniversalTest.php
@@ -7,6 +7,6 @@ class UniversalTest extends \PHPUnit_Framework_TestCase
     /** @test */
     function unifiedTheoryOfYolo()
     {
-        $this->assertTrue((bool) true);
+        $this->assertTrue(boolval(yolo\YOLO::class));
     }
 }
