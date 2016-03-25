761fbec0c60a6b55df4d291bbdfa6488cf6128b4
adeptofvoltron <adeptofvoltron@o2.pl>
2016-03-25 18:47:47 +0100

    hashtag my selfie

---
+++ src/selfie.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: praca
+ * Date: 25.03.16
+ * Time: 09:51
+ */
+
+namespace yolo;
+
+
+class selfie
+{
+    /**
+     * @var array
+     */
+    private $friends = [];
+    /**
+     * return your selfie
+     * @return $this
+     */
+    function __invoke()
+    {
+        return array_merge($this->friends, [new \ReflectionClass($this)]);
+    }
+
+    /**
+     * makes you a good selfie mood
+     */
+    function mood()
+    {
+        header('Location: https://www.youtube.com/watch?v=kdemFfbS5H0');
+        exit;
+    }
+
+    /**
+     * add new friends to your selfie
+     * @param mixed $friends
+     */
+    function friends($friends)
+    {
+        if(is_object($friends)) {
+            $this->add_friend($friends);
+        } else {
+            $this->add_friend((object)$friends);
+        }
+    }
+
+    private function add_friend($friend)
+    {
+        $this->friends[] = new \ReflectionClass($friend);
+    }
+
+
+}
